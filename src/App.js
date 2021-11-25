48d5da76ddb23efa5c8da2c227813feb87fcaf71
Gabriel Tosta <gabriel.tosta90@gmail.com>
2021-11-25 12:04:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Palmeira campeão da Liberta
         </a>
       </header>
     </div>
